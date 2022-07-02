5dd186c2300428428588491d6c858c6a07a0a28e
Wanderson Knight <wandersongasco@outlook.com>
2022-07-02 15:53:18 -0300

    Melhorias

--- web_scraping.py
+++ web_scraping.py
@@ -5,18 +5,21 @@ import sys
 
 print("Argumentos no Terminal: script.py nome_loja nome_produto")
 
-if len(sys.argv) > 2:
-    store =  ProductSearch(sys.argv[1].lower())
-else: 
+if len(sys.argv) > 3 and sys.argv[1].lower() == "mercado": 
     store = ProductSearch("mercadolivre")
+elif len(sys.argv) > 2:
+    store =  ProductSearch(sys.argv[1].lower())
+else:
+    print("Invalid arguments")
+    exit()
 
 store.store_list()
 store.start_selenium()
 
-if len(sys.argv) > 2:
-    store.search_input(sys.argv[2:])
+if sys.argv[1] == "mercado":
+    store.search_input(" ".join(sys.argv[3:]))
 else:
-    store.search_input("s20 fe")
+    store.search_input(" ".join(sys.argv[2:]))
 
 product_dict = store.get_products()
 store.best_products(product_dict)
